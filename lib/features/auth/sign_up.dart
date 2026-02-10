a15d5402b0cc2dfaf36029593b2a67060bdf72e0
Harry De Torres <box.devvv@gmail.com>
2026-02-10 23:12:17 +0800

    feat: Implement sign-up screen with form validation and UI components

---
+++ lib/features/auth/sign_up.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:pos/core/const/app_colors.dart';
+import 'package:pos/core/const/app_strings.dart';
+import 'package:pos/core/const/app_typography.dart';
+import 'package:pos/core/const/validators.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  AppStrings.signUpHeadline,
+                  style: AppTextStyles.headline(
+                    context,
+                  ).copyWith(color: AppColors.textPrimary, height: 2),
+                ),
+                Text(
+                  AppStrings.signUpSubHeadline,
+                  style: AppTextStyles.subtitle(context).copyWith(
+                    color: AppColors.textSecondary,
+                    height: 1.3,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 18),
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (v) => Validators.combine(v, [
+                    (x) => Validators.required(x, fieldName: "Email"),
+                    Validators.email,
+                  ]),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (v) => Validators.combine(v, [
+                    (x) => Validators.required(x, fieldName: "Password"),
+                    (x) => Validators.strongPassword(x, min: 8),
+                  ]),
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  spacing: 5,
+                  children: [
+                    Text(AppStrings.forgotPassword),
+                    GestureDetector(
+                      onTap: () {
+                        //TODO: Handle forgot password logic
+                      },
+                      child: Text(
+                        AppStrings.forgot,
+                        style: TextStyle(color: AppColors.brand),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        //TODO: Handle sign-up logic
+                      }
+                    },
+                    child: Text(AppStrings.signUp),
+                  ),
+                ),
+                const SizedBox(height: 12),
+
+                // TODO: create a  or divdier andd button for facebook and google buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(height: 1, color: AppColors.divider),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        'Or sign up with',
+                        style: AppTextStyles.caption(
+                          context,
+                        ).copyWith(color: AppColors.textSecondary),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(height: 1, color: AppColors.divider),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
